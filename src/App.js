9ca8df35c66802c45ab3458307ee37b7b5271489
Andrew Bagwell <abagwell90@gmail.com>
2019-04-08 18:49:48 -0400

    adding updated css

--- src/App.js
+++ src/App.js
@@ -11,17 +11,12 @@ class App extends Component {
     };
   }
 
-
-
-
-
   handleQueryChange = (e) => {
     this.setState({ query: e.target.value });
 
   }
   handleSearch = () => {
     console.log("Query: " + this.state.query);
-
 
     var url = 'https://www.googleapis.com/books/v1/volumes?q=' + this.state.query
     console.log(url);
@@ -39,8 +34,6 @@ class App extends Component {
     } else {
       const htmlBooks = [];
 
-     
-    
       books.forEach((book) => {
         htmlBooks.push(
           <div id="allbooks">
@@ -50,112 +43,80 @@ class App extends Component {
             <a href={book.saleInfo.buyLink}>Purchase link: {book.saleInfo.buyLink}</a>
 
             <div class="books">
-  <div class="bookdesign">
-  <img src={book.volumeInfo.imageLinks.thumbnail} alt="book image" />
-  </div>
-</div>
-           
+              <div class="bookdesign">
+                <img src={book.volumeInfo.imageLinks.thumbnail} alt="book image" />
+              </div>
+            </div>
           </div>)
       })
-    
 
       return <div>
         {htmlBooks}
-      </div>
-
-     
+            </div>
     }
-
   }
-
   render() {
     console.log("data", this.state.data.items);
     return (
-    
-<div>
+      <div>
+        <div class="component">
+          <ul class="align">
+            <li>
+              <figure class='book'>
 
+                <ul class='hardcover_front'>
+                  <li>
+                    <div class="coverDesign blue">
+                      <h1>CSS</h1>
+                      <p>BOOK</p>
+                    </div>
+                  </li>
+                  <li></li>
+                </ul>
 
+                <ul class='page'>
+                  <li></li>
+                  <li>
+                    <a class="btn">BookFinder APP!   </a> <p>Andrew J Bagwell</p>
+                  </li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
 
-<div class="component">
-				<ul class="align">
-					<li>
-						<figure class='book'>
+                <ul class='hardcover_back'>
+                  <li></li>
+                  <li></li>
+                </ul>
+                <ul class='book_spine'>
+                  <li></li>
+                  <li></li>
+                </ul>
+                <figcaption>
+                  <h1>BookFinder! App</h1>
+                  <span>Search any book you would like though Google Books</span>
 
+                </figcaption>
+              </figure>
+            </li>
+          </ul>
+        </div>
 
-							<ul class='hardcover_front'>
-								<li>
-									<div class="coverDesign blue">
-										<h1>CSS</h1>
-										<p>BOOK</p>
-									</div>
-								</li>
-								<li></li>
-							</ul>
+        <div className="topheader">
 
-				
+          <form class="searchbooks">
+            <input type="text" className="query" placeholder="Search a book ?" value={this.state.query} onChange={this.handleQueryChange} />
+            <button type="button" onClick={this.handleSearch}>Search</button>
+          </form>
+          {this.displayBooks(this.state.data.items)}
 
-							<ul class='page'>
-								<li></li>
-								<li>
-									<a class="btn">BookFinder APP!   </a> <p>Andrew J Bagwell</p>
-								</li>
-								<li></li>
-								<li></li>
-								<li></li>
-							</ul>
-
-						
-
-							<ul class='hardcover_back'>
-								<li></li>
-								<li></li>
-							</ul>
-							<ul class='book_spine'>
-								<li></li>
-								<li></li>
-							</ul>
-							<figcaption>
-								<h1>BookFinder! App</h1>
-								<span>Search any book you would like though Google Books</span>
-							
-							</figcaption>
-						</figure>
-					</li>
-				</ul>
-			</div>
-	
-    
-
-
-
-
-
-
-      <div className = "topheader">
-
-        <form class="searchbooks">
-          <input type="text" className="query" placeholder="Search a book ?" value={this.state.query} onChange={this.handleQueryChange} />
-          <button type="button" onClick={this.handleSearch}>Search</button>
-        </form>
-        {this.displayBooks(this.state.data.items)}
-        
-      </div>
-
-
-      <footer>
- BookFinder - Google Book Search App
- Andrew J Bagwell
-</footer>
-
-
-
-
-      </div>
-     
+        </div>
+        <footer>
+          BookFinder - Google Book Search App <br></br>
+          Andrew J Bagwell
+        </footer>
+         </div>
     );
   }
-
-
 }
-
 export default App;
